fb2b646907a5278b7557e3515d30b2913ed5a31f
underr1rrdog <gleb.koldunov@gmail.com>
2021-03-18 19:43:59 +0300

    add description_search

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -8,7 +8,9 @@ use App\Models\Collection;
 class SearchController extends Controller
 {     
     public function index(Request $request){
-        $collections = Collection::where('collection_name', 'like',"%$request->q%")->paginate(15);
+        $collections = Collection::where('collection_name', 'like',"%$request->q%")
+                                    ->orWhere('collection_description','like',"%$request->q%")->paginate(15);
+        //where('tag')
         return view('search.index',['collections'=>$collections]);
     }
 }
