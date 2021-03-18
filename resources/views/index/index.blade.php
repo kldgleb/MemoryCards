f0bea72ebc571e7be8172cd53722c4b8f0843818
underr1rrdog <gleb.koldunov@gmail.com>
2021-03-18 19:22:40 +0300

    descriptions now use

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -4,7 +4,7 @@
     <a href="{{route('index.create')}}">
     <div class="container p-5 mb-5 text-center border-add">
             <h1>Создать коллекцию карточек</h1>
-            <img src="/img/add.png" width="10%" height="10%"/>
+            <img class="myImg" src="/img/add.png">
     </div>
     </a>
     @if ($collections->first()) 
@@ -14,12 +14,14 @@
     <div class="container">
         <div class="row justify-content-center">
     @foreach ($collections as $collection)
-    
             <div class="col-3 p-5 m-2 h4 bg-dark text-white text-center"> 
-                <a href="{{route('index.show',[$collection->collection_name,0])}}">
-                {{$collection->collection_name}}
+                <a href="{{route('index.show',[$collection->collection_name,0])}}" id="showDescription">
+                    {{$collection->collection_name}}
                 </a>
-            </div>         
+                <a href="{{route('index.show',[$collection->collection_name,0])}}" id="target" style="display: none">
+                    {{$collection->collection_description}}
+                </a>
+            </div>                 
     @endforeach
         </div>
     </div>
@@ -27,3 +29,19 @@
     <h3 class="text-center"> Никто еще не создал свою коллекцию карточек, будьте первым</h3>
     @endif
 @endsection
+<script>
+    document.addEventListener("DOMContentLoaded", () => {
+    let showDescription = document.querySelectorAll("#showDescription");
+    let target = document.querySelectorAll("#target");
+    for(let i = 0;i < showDescription.length;i++){
+        showDescription[i].addEventListener("mouseover", function() {
+            showDescription[i].style.display = 'none';  
+            target[i].style.display = 'block';  
+        });
+        target[i].addEventListener("mouseout", function() {
+            target[i].style.display = 'none';
+            showDescription[i].style.display = 'block';
+        });
+    }
+});
+</script>
--- resources/views/index/showEnd.blade.php
+++ resources/views/index/showEnd.blade.php
@@ -6,6 +6,9 @@
         {{$collection->collection_name}}
     </a>
 </h1>
+<h3 class="text-muted text-center">
+    {{$collection->collection_description}}
+</h3>
 <div class="container p-5">
     <div class="row justify-content-center">
         <div class="col-2">
